93898c6eafd0f84969871143b0c94058316569c7
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-05-30 23:03:49 -0400

    dsf

--- client/src/components/Grid/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = ({ fluid, children }) => (
-  <div className={`footer${fluid ? "-fluid" : ""}`}>
-    {children}
-  </div>
-)
-
-export default Footer;
